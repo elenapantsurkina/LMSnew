7a34191b3655800944a6b160e52b2e2685d390f5
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-12-11 17:17:15 +0700

    в модель Course добавлены поля: количество уроков и инфо об уроках

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework.serializers import ModelSerializer
 from lms.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
+from rest_framework import serializers
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(ModelSerializer):
+    quantity_lessons = serializers.SerializerMethodField()
+    info_lessons = serializers.SerializerMethodField()
+
+    def get_quantity_lessons(self, obj):
+        return obj.lesson_set.count()
+
+    def get_info_lessons(self, obj):
+        lessons = obj.lesson_set.all()
+        return LessonSerializer(lessons, many=True).data
+
+    class Meta:
+        model = Course
+        fields = ["name", "description", "preview", "quantity_lessons", "info_lessons"]
