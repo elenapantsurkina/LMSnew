f3f60bbbb5c4b0ef58090a5880bc92145dac23b8
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-12-24 13:35:49 +0700

    реализованы тесты на просмотр .создание изменение и удаление уроков

--- lms/tests.py
+++ lms/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from lms.models import Course, Lesson
+from users.models import User
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        """Данные для теста(фикстура для теста)."""
+        self.user = User.objects.create(email="test@test.com")
+        self.course = Course.objects.create(name="Пайтон", description="Курс пайтон")
+        self.lesson = Lesson.objects.create(name="Урок пайтон", course=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse("lms:lessons_retrieve", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("name"), self.lesson.name)
+
+    def test_lesson_create(self):
+        url = reverse("lms:lessons_create")
+        data = {
+            "name": "Урок Пайтон 2",
+            "course": self.course.pk,
+            "video": "https://www.youtube.com",
+            "owner": self.user.pk,
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Lesson.objects.all().count(), 2)
+
+    def test_lesson_update(self):
+        url = reverse("lms:lessons_update", args=(self.lesson.pk,))
+        data = {
+            "name": "Пайтон нов"
+            }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("name"), "Пайтон нов")
+
+    def test_lesson_delete(self):
+        url = reverse("lms:lessons_delete", args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Lesson.objects.all().count(), 0)
--- lms/validators.py
+++ lms/validators.py
@@ -1,11 +1,9 @@
 from rest_framework.serializers import ValidationError
 
 
-link = "youtube.com"
-
 """Проверяет на использование ссылки только на yuotube.com."""
 
 
 def validate_link(value):
-    if value not in link:
+    if "youtube.com" not in value.lower():
         raise ValidationError("Ссылка на данный источник не допустима")
