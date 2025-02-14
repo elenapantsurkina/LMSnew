998ba554154d2e7e139a368fc7e6eaa05b978ec7
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-02-14 21:24:33 +0700

    проверка black isort

--- lms/views.py
+++ lms/views.py
@@ -1,11 +1,5 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-    get_object_or_404,
-)
+from rest_framework.generics import (CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView,
+                                     get_object_or_404)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
